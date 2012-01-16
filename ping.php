84df0edf203b4db247cf2e00227ee0302dda4e80
root <root@web.lax.ping.ms>
2012-01-15 23:14:59 -0800

    added version management

--- ping.php
+++ ping.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('common.php');
+
 $dest = $_GET['dest'];
 exec('sudo ping -c 4 -W 1 '.$dest,$output);
 echo implode("\n",$output);
--- trace.php
+++ trace.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('common.php');
+
 $dest = $_GET['dest'];
 exec('sudo traceroute -w 1 -q 1 '.$dest,$output);
 echo implode("\n",$output);
