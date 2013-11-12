9c1535505838518246814165514874f3801b78c4
Bryan Tong <contact@nullivex.com>
2013-11-12 16:06:28 -0700

    fixes #1
    This will validate domain/ip input using PHP's filter_var method

--- ping.php
+++ ping.php
@@ -3,5 +3,10 @@
 require_once('common.php');
 
 $dest = $_GET['dest'];
+
+//make sure the dest is sanitized as an IP or domain using PHP filters
+if(!filter_var(gethostbyname($dest),FILTER_VALIDATE_IP))
+    throw new Exception("Invalid domain name or IP passed")
+//actually make the ping request
 exec('sudo ping -c 4 -W 1 '.$dest,$output);
 echo implode("\n",$output);
